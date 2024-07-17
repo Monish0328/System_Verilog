5a89db4f88fcd51abfb6be5611c1f6a8717e0523
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-17 11:06:02 +0530

    Create 4_state_datatype.sv

---
+++ Data_types/4_state_datatype.sv
@@ -0,0 +1,12 @@
+module 4_state_datatype;
+    logic  [3:0] my_data;
+    logic   en;
+
+    initial begin
+      $display("my_data=0x%0h en=%0b",my_data,en);
+      my_data=4'hB;
+      $display("my_data=0x%0h en=%0b",my_data,en);
+      #1
+      $display("my_data=0x%0h en=%0b",my_data,en);
+    end
+endmodule
