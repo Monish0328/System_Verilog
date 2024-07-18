6e349bb81ee4daacba28c8e6d3f5903bec83fd73
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-18 15:20:05 +0530

    Create Two dimensional packed array.sv

---
+++ Data_types/Two dimensional packed array.sv
@@ -0,0 +1,11 @@
+
+module sd_array;
+  bit[7:0]  [3:0]  m_data;
+  initial begin
+    m_data=32'hface_cafe;
+    $display ("m_data=0x%0h",m_data);
+    for (int i=0;i<$size(m_data);i++) begin
+    $display ("m_data[%0d]=%b(ox%h)",i,m_data[i],m_data[i]);
+  end
+  end
+endmodule
