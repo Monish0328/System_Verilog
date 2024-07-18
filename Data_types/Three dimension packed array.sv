ca2fc9ada8ecc262cd1e47a05d623b640580a6d3
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-18 15:33:03 +0530

    Create Three dimension packed array.sv

---
+++ Data_types/Three dimension packed array.sv
@@ -0,0 +1,17 @@
+
+module thd_array;
+  bit [2:0] [3:0] [7:0]  m_data;
+  initial begin
+    m_data=32'hface_cafe;
+    m_data=32'h1234_5678;
+    m_data=32'hc0de_fade;
+    $display ("m_data=0x%0h",m_data);
+
+    foreach(m_data[i]) begin
+    $display ("m_data[%0d]=ox%h",i,m_data[i]);
+      
+      foreach(m_data[i][j]) begin
+        $display ("m_data[%0d][%0d]=ox%h",i,j,m_data[i][j]);
+  end
+  end
+endmodule
