d3016f7bb470fe17014015da389eef67249b517a
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-18 15:05:48 +0530

    Update Single dimension packed array.sv

--- Data_types/Single dimension packed array.sv
+++ Data_types/Single dimension packed array.sv
@@ -1,8 +1,10 @@
+
 module sd_array;
   bit[7:0]  m_data;
-  m_data=8'ha2;
-  for (int i=0;i<(m_size));i++) begin
-    $display ("m_data[%0d]=%0b",i,m_data[i])
+  initial begin
+    m_data=8'hA2;
+    for (int i=0;i<$size(m_data);i++) begin
+    $display ("m_data[%0d]=%0b",i,m_data[i]);
   end
   end
 endmodule
