0a2a8421705f56436cfc3cdaba92e1cb686832bc
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-18 15:42:53 +0530

    Update Three dimension packed array.sv

--- Data_types/Three dimension packed array.sv
+++ Data_types/Three dimension packed array.sv
@@ -2,9 +2,9 @@
 module thd_array;
   bit [2:0] [3:0] [7:0]  m_data;
   initial begin
-    m_data=32'hface_cafe;
-    m_data=32'h1234_5678;
-    m_data=32'hc0de_fade;
+    m_data[0]=32'hface_cafe;
+    m_data[1]=32'h1234_5678;
+    m_data[2]=32'hc0de_fade;
     $display ("m_data=0x%0h",m_data);
 
     foreach(m_data[i]) begin
