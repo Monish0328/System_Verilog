f512dcaff9be204563db4d811dc682ade01b521a
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-24 11:23:06 +0530

    Create Structure.sv

---
+++ Data_types/Structure.sv
@@ -0,0 +1,19 @@
+module structure_example;
+  struct{
+    String fruit,
+    int    count,
+    byte   expiry
+  } st_fruit;
+
+  initial begin 
+    st_fruit='{"apple",4,15};
+  $display("st_fruit=%p",st_fruit);
+
+  st_fruit.fruit="pineapple";
+  st_fruit.expiry="7";
+  
+  $display("st_fruit=%p",st_fruit);
+
+  end
+endmodule
+  
