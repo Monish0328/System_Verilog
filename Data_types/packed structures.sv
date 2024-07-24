b66607db58e007eb296356c81811746f091c682a
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-24 11:59:36 +0530

    Create packed structures.sv

---
+++ Data_types/packed structures.sv
@@ -0,0 +1,22 @@
+ typedef struct packed
+  {
+    bit [3:0] mode;
+    bit [2:0] cnfg;
+    bit [] en;
+  }st_ctrl;
+
+  module p_struct;
+    sr_ctrl ctrl_reg;
+
+    initial begin 
+      ctrl_reg ='{4'ha,3'h5,1};
+      $display ("ctrl_reg= %p",ctrl_reg);
+
+      ctrl_reg.mode=4'h3;
+      $display ("ctrl_reg= %p",ctrl_reg);
+      
+      ctrl_reg=8'hfa;
+       $display ("ctrl_reg= %p",ctrl_reg)
+
+    end
+  endmodule
