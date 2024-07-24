e362c07b635b3eed6aebb8d04e448672f4e1e348
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-24 11:50:14 +0530

    Create type def structures.sv

---
+++ Data_types/type def structures.sv
@@ -0,0 +1,22 @@
+module structure_example;
+typedef  struct{
+    String fruit;
+    int    count;
+    byte   expiry;
+  } st_fruit;
+
+  initial begin
+
+    st_fruit.fruit1='{"peach",5,10};
+    st_fruit.fruit2;
+    $display("fruit1=%p  fruit2=%p",fruit1,fruit2);
+    
+    fruit1=fruit2;
+    $display("fruit1=%p fruit2=%p",fruit1,fruit2);
+
+    fruit1.fruit="Orange";
+    $display("fruit1=%p  fruit2=%p",Fruit1,fruit2);
+
+  end
+endmodule
+  
