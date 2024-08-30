71aca5b7befe06b5c4e85bdc491c3b7b673aeaa1
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-08-30 10:15:33 +0530

    Create Without Soft Constraints.sv

---
+++ Constraints/Without Soft Constraints.sv
@@ -0,0 +1,21 @@
+class seq_item;
+  rand bit [7:0] val;
+
+  constraint val1_c {val inside{5,[10:15}};}
+endclass
+
+module constraint_example
+  seq_item item;
+  initial begin
+    item = new item();
+    repeat (5) begin
+    item.randomize();
+      $display("before inline constraint : val = %od",item.val);
+      item.randomize with { val inside {20:30}};};
+      $display("after inline constraint : val = %od",item.val);
+
+    end
+  end
+endmodule
+
+
